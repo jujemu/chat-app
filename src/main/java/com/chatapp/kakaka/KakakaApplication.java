ad29e0377407ec78aad44e0e7d78558bfe0edef8
Jemu Ju <jujemu30@gmail.com>
2024-05-04 18:58:47 +0900

    Init:

---
+++ src/main/java/com/chatapp/kakaka/KakakaApplication.java
@@ -0,0 +1,13 @@
+package com.chatapp.kakaka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakakaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakakaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chatapp/kakaka/KakakaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatapp.kakaka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakakaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
