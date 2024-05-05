e3c98bd73ef8ab24b4096657cab6960e0b0e61e9
Jemu Ju <jujemu30@gmail.com>
2024-05-06 02:59:24 +0900

    Fix: request DTO NoArgsConstructor 누락 해결

--- src/main/java/com/chatapp/kakaka/domain/user/controller/RegisterUserRequest.java
+++ src/main/java/com/chatapp/kakaka/domain/user/controller/RegisterUserRequest.java
@@ -3,9 +3,11 @@ package com.chatapp.kakaka.domain.user.controller;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class RegisterUserRequest {
 
     @NotBlank(message = "유저 이름은 필수값입니다.")
