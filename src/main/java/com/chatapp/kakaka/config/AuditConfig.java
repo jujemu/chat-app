d27fbf2e23c80b5173ae056598451b85e0389350
Jemu Ju <jujemu30@gmail.com>
2024-05-10 08:26:04 +0900

    Feat: init data.sql 추가 및 테스트 환경에서 실행되지 않도록 설정 추가

---
+++ src/main/java/com/chatapp/kakaka/config/AuditConfig.java
@@ -0,0 +1,9 @@
+package com.chatapp.kakaka.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class AuditConfig {
+}
