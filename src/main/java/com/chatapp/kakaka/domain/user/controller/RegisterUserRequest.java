4d780fa9f1eba1c84fab549f07b55f47ae7fbf15
Jemu Ju <jujemu30@gmail.com>
2024-05-09 10:34:57 +0900

    Feat: login request 수정
    UserService, UserController dto 수정 및 테스트 수정

--- src/main/java/com/chatapp/kakaka/domain/user/controller/RegisterUserRequest.java
+++ src/main/java/com/chatapp/kakaka/domain/user/controller/RegisterUserRequest.java
@@ -1,15 +1,18 @@
 package com.chatapp.kakaka.domain.user.controller;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@AllArgsConstructor
 @NoArgsConstructor
 public class RegisterUserRequest {
 
     @NotBlank(message = "유저 이름은 필수값입니다.")
     private String username;
+    private String uuid;
+
+    public RegisterUserRequest(String username) {
+        this.username = username;
+    }
 }
