3ac39d662833d780cf3d21a369bc22bef2591272
Jemu Ju <jujemu30@gmail.com>
2024-05-23 04:47:07 +0900

    Feat: Friends 목록 불러오기
    - 회원가입 시 자동으로 추가되는 플러스 친구들 조회

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -69,20 +69,50 @@ document.getElementById('loginForm')
 // -------------------------------  chat page  -------------------------------
 
 function chatPage() {
-    fetch("/users")
+    fetch("/friend/all/" + usernameGlobal, {
+        headers: {
+            'Authorization': "Basic " +
+                btoa(usernameGlobal + ":" + passwordGlobal)
+        }
+    })
         .then(response => response.json())
         .then(data => {
-            // 받은 데이터를 사용하여 HTML 리스트를 만듭니다.
-            const userListElement = document.getElementById("userList");
-            data["usernames"].forEach(username => {
-                const listItem = document.createElement("li");
-                listItem.textContent = username;
-                userListElement.appendChild(listItem);
-            });
+            const userListElement = document.getElementById("friends");
+            data["friends"]
+                .sort((a, b) => a["otherName"].localeCompare(b["otherName"]))
+                .forEach(friend => {
+                    const name = friend["otherName"];
+                    const type = friend["type"];
+                    const listItem = document.createElement("li");
+                    listItem.textContent = name;
+                    if (type === "PLUS") {
+                        const badge = document.createElement("span");
+                        badge.className = "badge badge-pill";
+                        badge.style.backgroundColor = "#198754"
+                        badge.textContent = "plus";
+                        listItem.appendChild(badge);
+                    }
+                    listItem.className = "list-group-item d-flex justify-content-between align-items-center";
+                    userListElement.appendChild(listItem);
+                });
+            selected();
         })
         .catch(error => {
             console.error("Error fetching user list:", error);
         });
+}
+
+function selected() {
+    const ul = document.getElementById('friends');
+    const items = ul.getElementsByTagName('li');
+    for (let i = 0; i < items.length; i++) {
+        items[i].addEventListener('mouseover', function () {
+            this.classList.add('active');
+        });
+        items[i].addEventListener('mouseout', function () {
+            this.classList.remove('active');
+        });
+    }
 }
 
 function participantsList() {
