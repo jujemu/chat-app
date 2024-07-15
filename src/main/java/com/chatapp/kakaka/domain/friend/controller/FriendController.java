cb205d38500f3faf192e226b310362431eef4769
Jemu Ju <jujemu30@gmail.com>
2024-07-16 06:14:09 +0900

    Feat: Sticky Session 을 활용하여 여러 인스턴스로의 스트림 문제 해결

--- src/main/java/com/chatapp/kakaka/domain/friend/controller/FriendController.java
+++ src/main/java/com/chatapp/kakaka/domain/friend/controller/FriendController.java
@@ -42,7 +42,7 @@ public class FriendController {
             throw new RestApiException(CommonErrorCode.INVALID_PARAMETER);
 
         // 클라이언트와 text/stream 연결된 직후에 메세지를 보내서 연결이 해제되지 않도록 한다.
-        SseEmitter sseEmitter = new SseEmitter(1000L * 120);
+        SseEmitter sseEmitter = new SseEmitter(1000L * 1);
         emitters.add(myName, sseEmitter);
         try {
             sseEmitter.send(SseEmitter.event()
